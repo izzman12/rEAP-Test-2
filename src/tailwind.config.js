a52a35bd45d7b400f810b4b10b99cf61cc4ae878
izzman12 <izzman12@gmail.com>
2025-07-19 17:16:41 -0400

    Update tailwind.config.js

--- src/tailwind.config.js
+++ src/tailwind.config.js
@@ -1,14 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{js,jsx,ts,tsx}", // Include all files in src
-    "./public/index.html"         // Optional, if you use Tailwind classes there
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./public/index.html"
   ],
   theme: {
     extend: {
       colors: {
         primary: '#1e40af', // Tailwind blue-800
-      }
+      },
+      fontFamily: {
+        inter: ['Inter', 'sans-serif'],
+      },
     },
   },
   plugins: [],
