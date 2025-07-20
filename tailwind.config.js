3a5c16f353d71e0d5f9df51bcbcd05e83d01d026
izzman12 <izzman12@gmail.com>
2025-07-20 10:25:50 -0400

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-  "./src/**/*.{js,jsx,ts,tsx}", 
-  "./public/index.html"
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./public/index.html"
   ],
   theme: {
     extend: {
       colors: {
-        primary: '#1e40af', // Custom blue
+        primary: '#1e40af', // Tailwind blue-800
       },
     },
   },
